4a16b577a6662632ccf676be4152b2f629602565
Timo Turnley <turnleytimo96@gmail.com>
2025-07-22 08:46:26 +0200

    Modified preprocess_TT,model_TT and created new file Train_TT

---
+++ neurocheck/ml_logic/Train_TT.py
@@ -0,0 +1,25 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from preprocess_TT import preprocess_eeg_data, filter_to_eeg_channels
+from model_TT import initialize_xgb_model, train_model, evaluate_model
+
+# Load raw data
+df = pd.read_csv('../raw_data/MEFAR_preprocessed/MEFAR_MID.csv')
+
+# Set target:
+y = df['class']
+
+# Set X and remove other features present in the MEFAR MID dataset.
+X = filter_to_eeg_channels(df)
+
+# Train_Test split
+X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2,random_state=42)
+
+# Initialize the model
+model = initialize_xgb_model()
+
+# Train and save
+train_model(X_train,y_train,model)
+
+# Evaluate
+evaluate_model(model, X_test,y_test)
